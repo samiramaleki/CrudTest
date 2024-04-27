9022237c0b93b36e624cfa2ba4d0686da0915567
Samira <Samira@MALEKI-PC>
2024-04-27 15:22:24 +0330

    refactor

--- Mc2.CrudTest.Application/CQRS/IndependentCreditNotes/Update/UpdateIndependentCreditNoteCommandValidator.cs
+++ Mc2.CrudTest.Application/CQRS/IndependentCreditNotes/Update/UpdateIndependentCreditNoteCommandValidator.cs
@@ -10,7 +10,7 @@ namespace Mc2.CrudTest.Application.CQRS.IndependentCreditNotes.Update
             RuleFor(c => c.IndependentCreditNoteInPutDto.CreditNumber.ToString()).Length(10).WithMessage("CreditNumber includes 10 digits.");
 
             RuleFor(c => c.IndependentCreditNoteInPutDto.ExternalCreditNumber).NotEmpty().WithMessage("ExternalCreditNumber can’t be empty");
-            RuleFor(c => c.IndependentCreditNoteInPutDto.ExternalCreditNumber.ToString()).Length(10).WithMessage("ExternalCreditNumber includes 10 characters.");
+            RuleFor(c => c.IndependentCreditNoteInPutDto.ExternalCreditNumber).Length(10).WithMessage("ExternalCreditNumber includes 10 characters.");
 
             RuleFor(c => c.IndependentCreditNoteInPutDto.TotalAmount).NotEmpty().WithMessage("TotalAmount It can’t be empty.");
             RuleFor(c => c.IndependentCreditNoteInPutDto.TotalAmount).GreaterThan(0).WithMessage("TotalAmount should be less than zero.");
